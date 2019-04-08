8c58f4faa27dbdbe53047cb5364d66ff44af7139
Noah Kahrs <42625318+nkahrs@users.noreply.github.com>
2019-04-07 22:46:28 -0400

    another section of paper

--- paper_2_chord_types.py
+++ paper_2_chord_types.py
@@ -1,7 +1,7 @@
 # 4/7/19: look at major/minor/dom7 and root scale degree
 
 import os, csv, statistics
-from kern_extract import parse_kern_file
+from kern_extract import parse_kern_file, pitchclass
 from graphing_utilities import *
 from major_minor import *
 
@@ -18,12 +18,12 @@ for root, dirs, files in os.walk(".\\corpus"):
         thisfilepath = root + '\\' + filename
         # print(thisfilepath)
         thisdata = parse_kern_file(thisfilepath, 16)
-        # ignore key
-        thisdata = thisdata[1]
-        # ignore duration
-        thisdata = [i[1] for i in thisdata]
-        # add to main collection
-        thedata += thisdata
+        # ignore duration and turn key into pc number
+        thisdata = (pitchclass(thisdata[0]), [i[1] for i in thisdata[1]])
+        # add to main collection, ignoring key
+        thedata += thisdata[1]
+        # add to separate collection with key
+        keysdata.append(thisdata)
 
 # sort all of our chords because we're not going to hear weird register changes
 # also remove duplicated pitches to avoid weirdness
@@ -41,35 +41,81 @@ def mtof(midinote):
     abovea4 = midinote - 69 # distance from A440
     return (440 * (2.0**(abovea4/12.0)))
 
-# Experiment 4a: separate into major/minor/dom7. do lo/hi/span
-print('Experiment 4a: major/minor')
+### Experiment 4a: separate into major/minor/dom7. do lo/hi/span
+##print('Experiment 4a: major/minor')
+##
+### extract M/m/7
+##experiments = [('Major',        list(filter(ismajor_pset, thedata))),
+##               ('Minor',        list(filter(isminor_pset, thedata))),
+##               ('Dominant 7th', list(filter( isdom7_pset, thedata)))
+##               ]
 
-# extract M/m/7
-experiments = [('Major',        list(filter(ismajor_pset, thedata))),
-               ('Minor',        list(filter(isminor_pset, thedata))),
-               ('Dominant 7th', list(filter( isdom7_pset, thedata)))
+##for thisexpt in experiments:
+##    print(thisexpt[0])
+##    for j in [0,1]: # 0 = MIDI, 1 = Hz
+##        if j:
+##            print('Hz')
+##        else:
+##            print('MIDI')
+##        thisdata = thisexpt[1]
+##        if j: # conversion to Hz
+##            thisdata = [[mtof(j) for j in i] for i in thisdata]
+##
+##        lowests = [i[0] for i in thisdata]
+##        highests = [i[-1] for i in thisdata]
+##        ranges = [(i[-1]-i[0]) for i in thisdata]
+##
+##        thecorrs = np.corrcoef([lowests, highests, ranges])
+##        for i in thecorrs:
+##                for j in i:
+##                        print(round(j,2), end='\t')
+##                print()
+##        print('\n')
+
+
+
+# Experiment 4b: lo/hi/span for each root scale degree
+print('Experiment 4b: scale degrees')
+
+# for each scaledegree, what height above key?
+heightsizes = [[0],    #^1
+               [1, 2], #^2
+               [3, 4], #^3
+               [5, 6], #^4
+               [7],    #^5
+               [8, 9], #^6
+               [10, 11] #^7
                ]
 
-for thisexpt in experiments:
-    print(thisexpt[0])
-    for j in [0,1]: # 0 = MIDI, 1 = Hz
-        if j:
-            print('Hz')
-        else:
-            print('MIDI')
-        thisdata = thisexpt[1]
-        if j: # conversion to Hz
-            thisdata = [[mtof(j) for j in i] for i in thisdata]
+for degree in range(7):
+    print("Scale Degree", degree+1, '\n')
 
-        lowests = [i[0] for i in thisdata]
-        highests = [i[-1] for i in thisdata]
-        ranges = [(i[-1]-i[0]) for i in thisdata]
+    # start structure
+    thisdata = []
+    
+    # keep only those chords whose lowest pc is the right scaledegree, and is size 3/4.
+    # I could do this more concisely...
+    for i in keysdata:
+        # i[0] is key, i[1] is list of chords
+        for j in i[1]:
+            if (len(j) in [3,4]) and (((j[0] - i[0]) % 12) in heightsizes[degree]):
+                thisdata.append(j)
 
-        thecorrs = np.corrcoef([lowests, highests, ranges])
-        for i in thecorrs:
-                for j in i:
-                        print(round(j,2), end='\t')
-                print()
-        print('\n')
+    # a hack... comment/uncomment this line because I already copy-pasted data
+    # this is for Hz conversion
+    thisdata = [[mtof(j) for j in i] for i in thisdata]
+
+    # nb copypasted from above, could refactor
+    lowests = [i[0] for i in thisdata]
+    highests = [i[-1] for i in thisdata]
+    ranges = [(i[-1]-i[0]) for i in thisdata]
+
+    thecorrs = np.corrcoef([lowests, highests, ranges])
+    for i in thecorrs:
+            for j in i:
+                    print(round(j,2), end='\t')
+            print()
+    print('\n')
+
 
 input()
